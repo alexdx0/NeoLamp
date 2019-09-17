7b12ecbc32166fc9150e2e7fc7bbd42964b978a8
alexdx0 <alexdx@mail.ru>
2019-09-17 23:47:21 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,115 @@
+/* import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+ */
+import React, {Fragment} from 'react';
+// import ReactDOM from 'react-dom';
+import SwipeableViews from 'react-swipeable-views';
+import Button from '@material-ui/core/Button';
+// import Slider from '@material-ui/core/Slider';
+// import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+import AppBar from '@material-ui/core/AppBar';
+import {useTheme} from '@material-ui/core/styles';
+
+export default function App() {
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
+
+  function TabPanel(props) {
+    const {children, value, index, ...other} = props;
+
+    return (
+      <Typography
+        component="div"
+        role="tabpanel"
+        hidden={value !== index}
+        id={`full-width-tabpanel-${index}`}
+        aria-labelledby={`full-width-tab-${index}`}
+        {...other}
+      >
+        <Box p={3}>{children}</Box>
+      </Typography>
+    );
+  }
+
+  function a11yProps(index) {
+    return {
+      id: `full-width-tab-${index}`,
+      'aria-controls': `full-width-tabpanel-${index}`,
+    };
+  }
+
+  function handleChangeIndex(index) {
+    setValue(index);
+  }
+
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <Fragment>
+      <Button variant="contained" color="primary">
+        Test button
+      </Button>
+
+      <AppBar position="static" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="fullWidth"
+          aria-label="full width tabs example"
+        >
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <SwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      >
+        <TabPanel value={value} index={0} dir={theme.direction}>
+          Item One
+        </TabPanel>
+        <TabPanel value={value} index={1} dir={theme.direction}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={value} index={2} dir={theme.direction}>
+          Item Three
+        </TabPanel>
+      </SwipeableViews>
+    </Fragment>
+  );
+}
